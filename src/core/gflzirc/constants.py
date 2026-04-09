18eadecb3d26665feddf1555d35276d50b36dfc6
xjt <969211735@qq.com>
2026-04-09 15:17:19 +0800

    Feat: En server

--- src/core/gflzirc/constants.py
+++ src/core/gflzirc/constants.py
@@ -4,7 +4,8 @@ SERVERS = {
     "AR15": "http://gfcn-game.bili.merge.sunborngame.com/index.php/5000",
     "SOP": "http://gfcn-game.ios.merge.sunborngame.com/index.php/3000",
     "RO635": "http://gfcn-game.ly.merge.sunborngame.com/index.php/4000",
-    "M16": "http://gfcn-game.tx.sunborngame.com/index.php/2000"
+    "M16": "http://gfcn-game.tx.sunborngame.com/index.php/2000",
+    "EN": "http://gf-game.sunborngame.com/index.php/1001",
 }
 
 # Crypto Keys
